13b447acdff4fedc9eb79af1e098796e2a7e2598
Lauren Cunningham <lauren.busbee@gmail.com>
2020-07-14 00:59:12 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -10,10 +10,10 @@ min_in_arrays = []
       if src[row_index][element_index] < min
         min = src[row_index][element_index]
       end
-      element_index += 1 
-      end
-    min_in_arrays << src[row_index][element_index]
-    row_index += 1 
+    element_index += 1 
+    end
+  min_in_arrays << src[row_index][element_index]
+  row_index += 1 
 end
 
 min_in_arrays
