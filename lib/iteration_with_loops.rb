61302514003b6b0b7d22764b2123d7ee025b04cf
Lauren Cunningham <lauren.busbee@gmail.com>
2020-07-14 01:07:53 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ min_in_arrays = []
   row_index = 0 
   while row_index < src.count do
     element_index = 0 
-    min = "100"
+    min = 100
     while element_index < src[row_index].count do
       if src[row_index][element_index] < min
         min = src[row_index][element_index]
